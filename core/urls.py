a7d36bc19f3502caea1d3992c6de955a56da41aa
rodrigo8064 <rodrigooliveiradel@gmail.com>
2026-06-07 22:06:55 -0300

    fix: working in easypanel

--- core/urls.py
+++ core/urls.py
@@ -5,6 +5,6 @@ from django.urls import path
 from .api import api
 
 urlpatterns = [
-    path('backoffice/'+settings.ADMIN_URL, admin.site.urls),
-    path('backoffice/api/', api.urls),
+    path(settings.ADMIN_URL, admin.site.urls),
+    path('api/', api.urls),
 ]
